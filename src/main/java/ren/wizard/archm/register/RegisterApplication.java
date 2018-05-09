09dde6ed1fbb3fe38dad4dd0c084b07e090410bd
chnyangjie <chnyangjie@163.com>
2018-05-09 15:42:09 +0800

    init

---
+++ src/main/java/ren/wizard/archm/register/RegisterApplication.java
@@ -0,0 +1,17 @@
+package ren.wizard.archm.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author uyangjie
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class RegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterApplication.class, args);
+	}
+}
